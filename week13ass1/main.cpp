b0f405484d22efff73a395c527dff0d01196c14a
fjl18042211 <61268428+fjl18042211@users.noreply.github.com>
2020-05-29 20:22:19 +0800

    week13ass1

--- week13ass1/main.cpp
+++ week13ass1/main.cpp
@@ -1,91 +1,99 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include<opencv2\opencv.hpp>
+
+using namespace std;
 using namespace cv;
 
+float O_distance(float * l, float * m, int n) {
+	int sum = 0;
+	for (int i = 0; i < n; i++) {
+		sum = sum + pow((*(l + i) - *(m + i)), 2);
+	}
+	return sum;
+}
 
-//为窗口标题定义的宏 
-#define WINDOW_NAME1 "【原始图片】"        
-#define WINDOW_NAME2 "【匹配窗口】"        
+void HOG(Mat & src, int blocksize, int nX, int nY, float * hist) {
+	Mat gx, gy;
+	Mat mag, angle;
+	Sobel(src, gx, CV_32F, 1, 0, 1);
+	Sobel(src, gy, CV_32F, 0, 1, 1);
+	cartToPolar(gx, gy, mag, angle, true);
+	int n = 0;
+	for (int i = 0; i < nY; i++) {
 
+		for (int j = 0; j < nX; j++) {
 
+			int n_x = j * blocksize, n_y = i * blocksize;
 
-Mat g_srcImage; Mat g_templateImage; Mat g_resultImage;
-int g_nMatchMethod;
-int g_nMaxTrackbarNum = 5;
-//函数声明
-void on_Matching(int, void*);
-void ShowHelpText();
-int main()
-{
+			for (int l = n_x; l < blocksize + n_x; l++) {
 
-	ShowHelpText();
-	g_srcImage = imread("D:\\img.png", 0);
-	g_templateImage = imread("D:\\template.png", 0);
-	//创建窗口
-	namedWindow(WINDOW_NAME1, WINDOW_AUTOSIZE);
-	namedWindow(WINDOW_NAME2, WINDOW_AUTOSIZE);
-	//创建滑动条进行算法选择
-	createTrackbar("算法", WINDOW_NAME1, &g_nMatchMethod, g_nMaxTrackbarNum, on_Matching);
-	on_Matching(0, 0);
+				for (int m = n_y; m < blocksize + n_y; m++) {
+
+					int region = (angle.at<float>(m, l)) / 40;
+					*(hist + region + n) += mag.at<float>(m, l);
+				}
+			}
+			n += 9;
+		}
+	}
+}
+
+int main() {
+
+	Mat srcMat = imread("D:\\template.png", 0);
+	Mat result = imread("D:\\img.png", 0);
+
+	if (!srcMat.data) {
+		cout << "fail" << endl;
+	}if (!result.data) {
+		cout << "fail2" << endl;
+	}
+
+	int blocksize = 16;
+	int nX = srcMat.cols / blocksize;
+	int nY = srcMat.rows / blocksize;
+	int bins = 9 * nX*nY;
+
+	double minR = 1e20;
+	int minX;
+	int minY;
+	int height = result.rows;
+	int width = result.cols;
+	int nheight = srcMat.rows;
+	int nwidth = srcMat.cols;
+
+	float * rhist = new float[bins];
+	memset(rhist, 0, sizeof(float)*bins);
+
+	HOG(srcMat, blocksize, nX, nY, rhist);
+
+	for (int i = 0; i < height - nheight + 1; i++) {
+
+		for (int j = 0; j < width - nwidth + 1; ) {
+
+			Rect rect(j, i, nwidth, nheight);
+			Mat iroi = result(rect);
+			float * roi_hist = new float[bins];
+			memset(roi_hist, 0, sizeof(float)*bins);
+			HOG(iroi, blocksize, nX, nY, roi_hist);
+
+			double distance = O_distance(rhist, roi_hist, bins);
+
+			if (distance < minR)
+			{
+				minR = distance;
+				minX = j;
+				minY = i;
+			}
+			delete[]  roi_hist;
+			j = j + 5;
+		}
+		i = i + 5;
+	}
+	delete[] rhist;
+	Rect rect(minX, minY, nwidth, nheight);
+	rectangle(result, rect, Scalar(0, 0, 255), 2, 8, 0);
+	imshow("result", result);
 	waitKey(0);
 	return 0;
-
-}
-
-
-
-void on_Matching(int, void*)
-{
-
-	Mat srcImage;
-	g_srcImage.copyTo(srcImage);
-
-	//初始化用于结果输出的矩阵
-	int resultImage_rows = g_srcImage.rows - g_templateImage.rows + 1;
-	int resultImage_cols = g_srcImage.cols - g_templateImage.cols + 1;
-	g_resultImage.create(resultImage_rows, resultImage_cols, CV_32FC1);
-
-
-	matchTemplate(g_srcImage, g_templateImage, g_resultImage, g_nMatchMethod);
-	normalize(g_resultImage, g_resultImage, 0, 1, NORM_MINMAX, -1, Mat());
-
-	//匹配的位置定位
-	double minValue; double maxValue; Point minLocation; Point maxLocation;
-	Point matchLocation;
-	minMaxLoc(g_resultImage, &minValue, &maxValue, &minLocation, &maxLocation, Mat());
-
-
-	if (g_nMatchMethod == TM_SQDIFF || g_nMatchMethod == TM_SQDIFF_NORMED)
-	{
-		matchLocation = minLocation;
-	}
-	else
-	{
-		matchLocation = maxLocation;
-	}
-
-	//绘制出矩形，并显示最终结果
-	rectangle(srcImage, matchLocation, Point(matchLocation.x + g_templateImage.cols, matchLocation.y + g_templateImage.rows), Scalar(0, 0, 255), 2, 8, 0);
-	rectangle(g_resultImage, matchLocation, Point(matchLocation.x + g_templateImage.cols, matchLocation.y + g_templateImage.rows), Scalar(0, 0, 255), 2, 8, 0);
-
-	imshow(WINDOW_NAME1, srcImage);
-	imshow(WINDOW_NAME2, g_resultImage);
-
-}
-
-
-
-
-static void ShowHelpText()
-{
-
-
-	printf("\n\n\t请调整滑动条观察图像效果\n\n");
-	printf("\n\t滑动条对应的方法数值说明: \n\n"
-		"\t\t方法【0】- 平方差匹配法(SQDIFF)\n"
-		"\t\t方法【1】- 归一化平方差匹配法(SQDIFF NORMED)\n"
-		"\t\t方法【2】- 相关匹配法(TM CCORR)\n"
-		"\t\t方法【3】- 归一化相关匹配法(TM CCORR NORMED)\n"
-		"\t\t方法【4】- 相关系数匹配法(TM COEFF)\n"
-		"\t\t方法【5】- 归一化相关系数匹配法(TM COEFF NORMED)\n");
 }
